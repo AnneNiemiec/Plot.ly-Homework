5166d9b8fed407d74f921255e75e3df6b7047498
AnneNiemiec <anne_niemiec@outlook.com>
2021-06-28 22:15:57 -0700

    Bubble format

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -9,7 +9,13 @@ d3.json("samples.json").then((incomingData) => {
   var filteredDemo = sampleSet.filter(Demo);
   var demoGraphics=filteredDemo[0]
   var salt=d3.select("#sample-metadata");
-  salt.html("")
+  salt.html("");
+
+// Prints 
+Object.entries(demoGraphics).forEach(entry => {
+  const [key, value] = entry;
+  salt.append('p').text(`${key}:${value}`);
+});
   });
 }
 // Use d3.json() to fetch data from JSON file
@@ -22,22 +28,21 @@ d3.json("samples.json").then((incomingData) => {
   // Use filter() to pass the function as its argument
   var filteredButtons = sampleSet.filter(filterbellyButtons);
 
-    //console.log(incomingData);
+  //console.log(incomingData);
     console.log(filteredButtons);
    
    var sampleInfo = filteredButtons[0];
    console.log(sampleInfo);
 
-   // Grabbing data
+   // Grab data
    var Coronado=sampleInfo.otu_ids;
    var Beach=sampleInfo.sample_values;
    var Sand=sampleInfo.otu_labels;
-   var Sun=Coronado.slice(0,10).reverse()
+   var Sun=Coronado.slice(0,10).map(otu_id=>otu_id).reverse()
   
    // Define the plot layout
    var layout = {
      title: "bacteria"
-   // format here!!!
    };
 
   // Create your trace.
@@ -52,27 +57,52 @@ d3.json("samples.json").then((incomingData) => {
    Plotly.newPlot("bar", trace, layout);
    console.log(Beach.slice(0,10).reverse());
 
-   // Plot bubble chart !!!
+   // Plot bubble chart
+      // Grab data
+      var Red=sampleInfo.otu_ids;
+      var White=sampleInfo.sample_values;
+      var Blue=sampleInfo.otu_labels;
+      var Green=Red.slice(0,10).reverse()
+     
+      // Define the plot layout
+      var layout = {
+        title: "x"
+       };
+   
+     // Create your trace.
+     var trace = [{
+       x: Red,
+       y: White,
+       text: Red,
+       mode: "markers",
+       marker: {
+         size: White, color: Red
+       }
+     }];
+      // Plot the bubble chart 
+      Plotly.newPlot("bubble", trace, layout);
+      console.log(White.slice(0,10).reverse());
 });
 };
 // Use d3.json() to fetch data from JSON file
 function bellies(){
-    var umbrella=d3.select("#selDataset");
+    var purple=d3.select("#selDataset");
     d3.json("samples.json").then((incomingData) => {
-      var shovel=incomingData.names;
+      var orange=incomingData.names;
 
 //Append data
-shovel.forEach(element => {
-    umbrella.append('option').text(element).property("value");
+orange.forEach(element => {
+    purple.append('option').text(element).property("value");
 });
-biodiversity(shovel[0])
-
+biodiversity(orange[0])
+metadata(orange[0])
 });
 };
 
 // New function
 function optionChanged(fish){
   biodiversity(fish)
+  metadata(fish)
 }
 bellies();
 
